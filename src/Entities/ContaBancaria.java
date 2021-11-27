8752edbec1c47797d44bdd816ed3bd0ed1f1cc75
XxDiogoX <diogorodriguesk02@gmail.com>
2021-11-27 04:07:35 -0300

    Criada a classe ContaBancaria com seus 3 atributos e metodos GET e SET

---
+++ src/Entities/ContaBancaria.java
@@ -0,0 +1,34 @@
+package Entities;
+
+public class ContaBancaria {
+	private int numberCount;
+	private String nameHolder;
+	private double saldo;
+	
+	public int getNumberCount() {
+		return numberCount;
+	}
+	
+	public void setNumberCount(int numberCount) {
+		this.numberCount = numberCount;
+	}
+	
+	public String getNameHolder() {
+		return nameHolder;
+	}
+	
+	public void setNameHolder(String nameHolder) {
+		this.nameHolder = nameHolder;
+	}
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	
+
+}
